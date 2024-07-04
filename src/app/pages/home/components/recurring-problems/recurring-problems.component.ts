a1aa3ebc2cb097b88a49720c528678bf167cfb5d
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-07-04 12:17:27 -0300

    feat: Recurring problems section

---
+++ src/app/pages/home/components/recurring-problems/recurring-problems.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recurring-problems',
+  standalone: true,
+  imports: [],
+  templateUrl: './recurring-problems.component.html',
+  styleUrl: './recurring-problems.component.css'
+})
+export class RecurringProblemsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RecurringProblemsComponent } from './components/recurring-problems/recurring-problems.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, RecurringProblemsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
