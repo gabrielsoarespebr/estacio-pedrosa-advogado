898f27bd4367b8de0569e96fdcddb6bc284a56d9
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-07-05 12:41:23 -0300

    refactor: About section - Image size and CTA button

--- src/app/pages/home/components/about/about.component.ts
+++ src/app/pages/home/components/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CtaButtonComponent } from '../../../../shared/components/cta-button/cta-button.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CtaButtonComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
