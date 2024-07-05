42d68acabe78c0823109ca32c9a9961894ce81d8
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-07-05 17:19:53 -0300

    feat: Back to top button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,16 @@
 import { AboutComponent } from './components/about/about.component';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecurringProblemsComponent } from './components/recurring-problems/recurring-problems.component';
 import { CompetitiveEdgesComponent } from './components/competitive-edges/competitive-edges.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     BannerComponent,
     RecurringProblemsComponent,
@@ -18,4 +20,15 @@ import { CompetitiveEdgesComponent } from './components/competitive-edges/compet
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  showBackToTop: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const verticalOffset =
+      document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const windowHeight = window.innerHeight;
+
+    this.showBackToTop = verticalOffset > windowHeight / 2;
+  }
+}
